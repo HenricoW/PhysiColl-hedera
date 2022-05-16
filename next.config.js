ddd4608f058ba8cae5fadc82fc1ae3016e2ea21e
HenricoW <henrico.ashley.White@gmail.com>
2022-05-16 12:05:36 +0200

    chore: hashconnect setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const withTM = require("next-transpile-modules")(["hashconnect"]);
+
+module.exports = withTM({});
